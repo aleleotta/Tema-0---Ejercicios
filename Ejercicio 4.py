3cf8bfc81f751bf8c47a4eebec009ea14cfee283
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-21 21:01:28 +0200

    .

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -3,3 +3,13 @@ Codificar el juego �el n�mero secreto�, que consiste en acertar un n�mer
 Para ello se introduce por teclado una serie de n�meros, para los que se indica: �mayor� o �menor�, seg�n sea mayor o menor con respecto al n�mero secreto.
 El proceso termina cuando el usuario acierta o cuando se rinde (introduciendo un -1).
 """
+import random
+guessnumber = random.randint(0, 100)
+print("Random number:", str(guessnumber))
+userNum = 0
+while userNum!=-1 and userNum!=guessnumber:
+    userNum = int(input("Guess number: "))
+    if userNum==-1:
+        print("Good luck next time!")
+        break
+    print("You guessed!") if userNum==guessnumber else print("Wrong!")
--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,3 +1,7 @@
 """
 Escribir una aplicaci�n para aprender a contar, que pedir� un n�mero n y mostrar� todos los n�meros del 1 al n.
 """
+
+n = int(input("Give a random number: "))
+for currentNum in range(n+1):
+    print(currentNum, end=" ")
