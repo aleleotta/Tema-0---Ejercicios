6ac8d23cdb11f1d2ab1c2958a8f1affadf220aa2
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-25 14:26:53 +0200

    .

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,3 +1,9 @@
 """
 Pedir un n�mero y calcular su factorial. Por ejemplo, el factorial de 5 se denota 5! y es igual a 5x4x3x2x1 = 120.
 """
+
+num = int(input("Introduce a number: "))
+result = num
+for i in range(num-1, 1, -1):
+    result = result * i
+print("Result:", result)
