98a1d175c12b31e25ccfbe6a2a9862ae22451e9d
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-25 13:26:04 +0200

    .

--- Ejercicio 9/Funciones.py
+++ Ejercicio 9/Funciones.py
@@ -2,3 +2,7 @@
 Escribe una funci�n a la que se le pasen dos enteros y muestre todos los n�meros comprendidos entre ellos.
 Desde el m�todo main() lee los dos n�meros enteros, los cuales deben introducirlos el usuario, y p�salos como par�metros de entrada de la funci�n.
 """
+
+def printBetweenNums(num1, num2):
+    for i in range(num1, num2+1):
+        print(i, end=" ")
---
+++ Ejercicio 9/Main.py
@@ -0,0 +1,7 @@
+from Funciones import printBetweenNums
+
+print("Welcome! Please introduce two numbers to show all numbers between them.")
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+printBetweenNums(num1, num2)
+print("\nHave a great day!")
